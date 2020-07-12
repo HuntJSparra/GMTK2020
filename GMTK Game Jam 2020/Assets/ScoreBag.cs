af67b6771bf7627c065d1666911d7176b63714eb
HuntJSparra <huntehlaboratories@gmail.com>
2020-07-12 11:18:53 -0400

    Added victory screen

---
+++ GMTK Game Jam 2020/Assets/ScoreBag.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ScoreBag : MonoBehaviour
+{
+    public TextMeshProUGUI nameGUI;
+    public TextMeshProUGUI titleGUI;
+    public TextMeshProUGUI scoreGUI;
+
+    public void Initialize(string name, string title, int score)
+    {
+        nameGUI.text = name;
+        titleGUI.text = title;
+        scoreGUI.text = score.ToString();
+    }
+}
